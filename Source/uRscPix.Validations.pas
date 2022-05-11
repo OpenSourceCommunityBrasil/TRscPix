6644571ebfe5ba035d35c2d036381bcaafb3c13c
Roniery Santos Cardoso <ronierys2@hotmail.com>
2022-05-11 05:37:39 -0400

    Atualização da expressão relugar que valida o número de telefone da chave pix, deve-se informar o número completo desde o DDDI. Exemplo: +55 88 9 5555 4444 -> +5592984391279
    se o número informado não seguir essa estrutura ira retorna um erro ao tentar executar alguma função do componente.

--- Source/uRscPix.Validations.pas
+++ Source/uRscPix.Validations.pas
@@ -62,9 +62,9 @@ begin
   Result := False;
 
   { Recuperando somente os numeros }
-  vFone := GetStrNumber(aCelNumber);
+  vFone :=  Copy(aCelNumber, 1, 1)  +  GetStrNumber(aCelNumber);
   try
-    ipRegExp := '^[1-9]{2}(?:[6-9]|9[1-9])[0-9]{3}[0-9]{4}$';
+    ipRegExp := '^\+[1-9]{2}[1-9]{2}(?:[6-9]|9[1-9])[0-9]{3}[0-9]{4}$';
     if TRegEx.IsMatch(vFone, ipRegExp) then
       Result := True;
   except
